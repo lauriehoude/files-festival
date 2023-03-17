0ffc32f8d4096ed8e8cf84d3e0203811f583fd4f
Kye Moon <jessyboustany2013@gmail.com>
2023-03-16 20:45:35 -0400

    awa

--- 02_js/script.js
+++ 02_js/script.js
@@ -1,4 +1,3 @@
-
 const mySwiperlp = new Swiper(swiperContainer, parameters);
 
 
@@ -6,30 +5,32 @@ const swiper = new Swiper('.swiperarts', {
     direction: 'horizontal',
     grabCursor: true,
     loop: true,
-    
-      slidesPerView: 1,
+
+    slidesPerView: 1,
     spaceBetween: 10,
     // Responsive breakpoints
     breakpoints: {
-      // when window width is >= 320px
-      768: {
-        slidesPerView: 2,
-        spaceBetween: 25
-      },
-      // when window width is >= 480px
-      992: {
-        slidesPerView: 3,
-        spaceBetween: 30
-      },
-  
-    }
-  
-  
-  });
-   
-  
+        // when window width is >= 320px
+        768: {
+            slidesPerView: 2,
+            spaceBetween: 25
+        },
+        // when window width is >= 480px
+        992: {
+            slidesPerView: 3,
+            spaceBetween: 30
+        },
 
-const swiperlp = new Swiper("swiper-screenshots", {
+    }
+
+
+});
+
+
+
+
+const swiperlp = new Swiper(".swiper-screenshots", {
+
     direction: "horizontal",
     loop: true,
     centeredSlides: true,
