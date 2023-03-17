59101823c365a51b055c11a3d882b33377a34e78
lauriehoude <2052728@cmontmorency.qc.ca>
2023-03-16 20:07:00 -0400

    ki

--- 02_js/script.js
+++ 02_js/script.js
@@ -1,1 +1,17 @@
-const mySwiperlp = new Swiper(swiperContainer, parameters);
+const swiper = new Swiper(".swiper-screenshot", {
+    direction: "horizontal",
+    loop: true,
+    centeredSlides: true,
+    navigation: {
+        nextEl: ".swiper-button-next",
+        prevEl: ".swiper-button-prev",
+    },
+    slidesPerView: 1,
+    spaceBetween: 20,
+    breakpoints: {
+        1200: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+    },
+});
