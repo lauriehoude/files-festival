e38c3ecf754c1260a28b6c7303c9a9d6eb2581a7
Jessica Boustany <jessyboustany2013@gmail.com>
2023-03-16 23:17:14 -0400

    time to commit suicide instead of github

--- 02_js/script.js
+++ 02_js/script.js
@@ -29,3 +29,16 @@ const swiperapp = new Swiper(".swiper-filters", {
         enable: true,
     },
 });
+
+
+const swiperappartworks = new Swiper(".swiper-featured-artworks", {
+    direction: "horizontal",
+    grabCursor: true,
+    freeMode:{
+        enable: true,
+    },
+      slidesPerView: 2.1,
+    spaceBetween: 10,
+  
+  
+});
