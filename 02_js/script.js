9dc1b1ca975e64dfbbb1b92db76c30302310b2ce
Kye Moon <jessyboustany2013@gmail.com>
2023-03-16 20:38:41 -0400

    awa

--- 02_js/script.js
+++ 02_js/script.js
@@ -1,1 +1,30 @@
 const mySwiperlp = new Swiper(swiperContainer, parameters);
+
+
+const swiper = new Swiper('.swiperarts', {
+    direction: 'horizontal',
+    grabCursor: true,
+    loop: true,
+    
+      slidesPerView: 1,
+    spaceBetween: 10,
+    // Responsive breakpoints
+    breakpoints: {
+      // when window width is >= 320px
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 25
+      },
+      // when window width is >= 480px
+      992: {
+        slidesPerView: 3,
+        spaceBetween: 30
+      },
+  
+    }
+  
+  
+  });
+   
+  
+  
